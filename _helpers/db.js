33b89cbe6643399be5e7f58e681a5f774722c5a3
kimeriksson75 <kimeriksson75@gmail.com>
2020-05-06 19:34:45 +0200

    mongoose connect catch error

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,7 +2,14 @@ const config = require('config.json');
 const mongoose = require('mongoose');
 
 // connect  
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('DB connection established');
+  })
+  .catch((error) => {
+    console.error(`DB connection faild: ${error}`)
+    throw new Error('Could not connect to DB')
+  })
 mongoose.Promise = global.Promise;
 
 module.exports = {
