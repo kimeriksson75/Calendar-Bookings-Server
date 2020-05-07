5f4c03e1a9f572f1283b1dc01f8be9ab43d7e63d
kimeriksson75 <kimeriksson75@gmail.com>
2020-05-07 11:07:54 +0200

    heroku test

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const helmet = require('helmet');
+
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
-const helmet = require('helmet');
 
-app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(helmet());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(jwt());
 
 // Add a health check route in express
@@ -23,11 +24,11 @@ app.get('/_health', (req, res) => {
 app.get('/', function (req, res) {
   res.status(200).send('ok')
 });
-app.use('/users', require('./users/user.controller'));
-app.use('/bookings', require('./bookings/booking.controller'));
-app.use('/apartments', require('./apartments/apartment.controller'));
+// app.use('/users', require('./users/user.controller'));
+// app.use('/bookings', require('./bookings/booking.controller'));
+// app.use('/apartments', require('./apartments/apartment.controller'));
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.port || 8080) : 3000;
 
