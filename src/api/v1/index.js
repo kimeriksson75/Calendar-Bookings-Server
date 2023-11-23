8b695aeed3146c2929694a16f46208680e201d29
kimeriksson75 <kimeriksson75@gmail.com>
2023-11-23 15:08:43 +0100

    health message

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const jwt = require("../../_helpers/jwt");
 
 router.get("/_health", (req, res) => {
-  res.status(200).json({ status: "ok" });
+  res.status(200).json({ status: "alive and kick'n" });
 });
 router.get("/", function (req, res) {
   res.status(200).send("ok");
 });
 router.get("/verify-access-token", jwt.authenticateToken, (req, res) =>
-  res.status(200).json({ status: "alive and kick'n" }),
+  res.status(200).json({ status: "ok" }),
 );
 router.use("/users", require("./users/user.routes"));
 router.use(
